e917d84df472bc2e883e23b25f96670d56760ece
Marijn van der Werf <marijn.vanderwerf@gmail.com>
2021-09-29 09:00:31 +0200

    Add search page

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,12 @@
 <?php
 declare(strict_types=1);
 
+use Fig\Http\Message\StatusCodeInterface;
 use Marijnvdwerf\Peek\ArticleRepository;
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Exception\HttpNotFoundException;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
 use Slim\Views\Twig;
 
 return function (App $app) {
@@ -14,6 +15,33 @@ return function (App $app) {
         $articles = $articleRepo->all();
         $twig = Twig::fromRequest($request);
         return $twig->render($response, 'index.twig', [
+            'articles' => $articles
+        ]);
+    });
+
+    $app->get('/search', function (Request $request, Response $response) {
+        if (!isset($request->getQueryParams()['q'])) {
+            return $response
+                ->withHeader('Location', '/')
+                ->withStatus(StatusCodeInterface::STATUS_FOUND);
+        }
+
+        $query = $request->getQueryParams()['q'];
+        $tokens = preg_split('/\s+/', $query);
+        $tokens = array_filter($tokens);
+
+        if (empty($tokens)) {
+            return $response
+                ->withHeader('Location', '/')
+                ->withStatus(StatusCodeInterface::STATUS_FOUND);
+        }
+
+        $articleRepo = $this->get(ArticleRepository::class);
+        $engine = new \Marijnvdwerf\Peek\SearchEngine($articleRepo->all());
+        $articles = $engine->search(...$tokens);
+        $twig = Twig::fromRequest($request);
+        return $twig->render($response, 'search.twig', [
+            'query' => $query,
             'articles' => $articles
         ]);
     });
